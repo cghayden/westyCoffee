05183ef2295a0cac1eab949978a0b12fa74053c0
Corey Hayden <cghayden@gmail.com>
2021-04-05 13:22:02 -0400

    query home page data from client side
    - read only text block names to enable query to sanity

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,8 @@ import { graphql } from 'gatsby';
 import SEO from '../components/SEO';
 import styled from 'styled-components';
 import CoffeeCard from '../components/CoffeeCard';
-import HomePageText from '../components/HomePageText';
+import useLatestHomePageData from '../utils/useLatestHomePageData';
+// import HomePageText from '../components/HomePageText';
 
 const HomeMainStyles = styled.main``;
 const CoffeeDisplay = styled.div`
@@ -21,24 +22,23 @@ const HomePageTextStyles = styled.div`
   }
 `;
 
-export default function homePage({ data }) {
+export default function HomePage() {
+  const { featuredCoffee, homePageLead } = useLatestHomePageData();
   // const content = data.textQuery.nodes[0];
-  const text = data.textQuery.nodes[0].content;
-  console.log('text', text);
+  // const text = data.textQuery.nodes[0].content;
   return (
     <>
       <SEO title={'Neighborly Coffee'} />
       <HomeMainStyles>
         <h2>Our Roasts of the Week</h2>
-        {/* <HomePageText /> */}
         <HomePageTextStyles>
-          {text.map((entry, i) => (
-            <p key={i}>{entry._rawChildren[0].text}</p>
+          {homePageLead?.contentRaw.map((entry) => (
+            <p key={entry._key}>{entry.children[0].text}</p>
           ))}
         </HomePageTextStyles>
         <CoffeeDisplay>
-          {data.coffees.nodes.map((coffee) => (
-            <CoffeeCard key={coffee.id} coffee={coffee} />
+          {featuredCoffee?.map((coffee) => (
+            <CoffeeCard key={coffee._id} coffee={coffee} />
           ))}
         </CoffeeDisplay>
       </HomeMainStyles>
@@ -46,27 +46,27 @@ export default function homePage({ data }) {
   );
 }
 
-export const query = graphql`
-  query {
-    coffees: allSanityCoffee {
-      nodes {
-        id
-        name
-        price
-        region
-        roastLevel
-        description
-        grade
-      }
-    }
-    textQuery: allSanityTextBlock(filter: { name: { eq: "Home Page Lead" } }) {
-      nodes {
-        name
-        heading
-        content {
-          _rawChildren
-        }
-      }
-    }
-  }
-`;
+// export const query = graphql`
+//   query {
+//     coffees: allSanityCoffee {
+//       nodes {
+//         id
+//         name
+//         price
+//         region
+//         roastLevel
+//         description
+//         grade
+//       }
+//     }
+//     textQuery: allSanityTextBlock(filter: { name: { eq: "Home Page Lead" } }) {
+//       nodes {
+//         name
+//         heading
+//         content {
+//           _rawChildren
+//         }
+//       }
+//     }
+//   }
+// `;
