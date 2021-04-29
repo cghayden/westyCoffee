925aa9969174281a4fb3245a295640fdc31dced2
Corey Hayden <cghayden@gmail.com>
2021-04-29 13:05:59 -0400

    basic events page setup

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,10 +1,39 @@
 import React from 'react';
-
-export default function eventsPage() {
+import { graphql } from 'gatsby';
+import { mapEdgesToNodes } from '../utils/helpers';
+import SEO from '../components/SEO';
+import EventList from '../components/EventList';
+export default function eventsPage({ data, errors }) {
+  if (errors) {
+    return <GraphQLErrorList errors={errors} />;
+  }
+  const eventNodes = (data || {}).events ? mapEdgesToNodes(data.events) : [];
   return (
-    <main>
-      <h1>Events Page</h1>
-      <p>Stay Tuned!</p>
-    </main>
+    <>
+      <SEO title={'Events'} />
+      <h1 className='pageHeader'>Events</h1>
+      <main>{eventNodes && <EventList nodes={eventNodes} />}</main>
+    </>
   );
 }
+
+export const query = graphql`
+  query EventsPageQuery {
+    events: allSanityEvent(sort: { fields: date, order: ASC }) {
+      edges {
+        node {
+          id
+          title
+          date
+          _rawDescription
+          addressLine1
+          addressLine2
+          city
+          state
+          zipCode
+          location
+        }
+      }
+    }
+  }
+`;
